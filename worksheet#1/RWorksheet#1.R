fb87305c280e23705d8ed0ccff42a070c83effab
Jocedel Garnette Doronila <jocedelgarnettedoronila@students.isatu.edu.ph>
2023-12-22 11:44:13 +0000

    Eh

--- worksheet#1/RWorksheet#1.R
+++ worksheet#1/RWorksheet#1.R
@@ -1,64 +1,74 @@
-age <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,51, 35, 24, 33, 41)
-
+# 1.
+# a.
+age <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41, 51, 35, 24, 33, 41)
+# b.
 length(age)
-
-#2
-reciprocalAge <-1/age
-reciprocalAge
-
-#3 
+# 2.
+1/age
+# 3.
 new_age <- c(age, 0, age)
-new_age
-
-#4 
+# 4.
 sort(age)
-
-#5
+# 5.
 min(age)
 max(age)
 
-#6
-Data <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.4, 2.7)
+# 6.
+# a.
+data <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.4, 2.7)
 
-#7
-Doubledata <- (Vdata * 2)
-Doubledata
+# b.
+length(data)
 
-#8
-Integers <- seq(integer(length=100))
-Integers
-Numbers <-seq(20,60)
-Numbers
-mean(Numbers)
-Sum <- sum(seq(51,91))
-Sum
-SecondIntegers <- seq(integer(length= 1000))
-SecondIntegers
-#9
-IntegersThird <- filter(function(i){all(i %% c(3,5,7) != 0}), seq(integer(length = 100))
-IntegersThird
-#10
-Reversedsequence <- rev(Integers)
-Reversedsequence
-#11
-UnderTwentyFive <- c(3, 5, 6, 9, 10, 12, 15, 18, 20, 21, 24, 25)
-Twosum <- sum(UnderTwentyFive)
-Twosum
-UnderTwentyFive
-#12
-{x <- 0 + x + 5 +}
-#13
+# 7.
+data * 2
+
+# 8.1
+seq(1, 100)
+
+# 8.2.
+seq(20, 60)
+
+# 8.3.
+mean(seq(20, 60))
+
+# 8.4.
+sum(seq(51, 91))
+
+# 8.5.
+length(seq(1, 1000))
+
+# 9.
+filter(function(i) { all(i %% c(3, 5, 7) != 0) }, seq(100))
+
+# 10.
+seq(100, 1, by = -1)
+
+# 11.
+nums <- seq(1, 25)
+selected_nums <- nums[nums %% 3 == 0 | nums %% 5 == 0]
+sum(selected_nums)
+
+# 12.
+{ x <- 0 + x + 5 }
+
+# 13.
 score <- c(72, 86, 92, 63, 88, 89, 91, 92, 75, 75, 77)
 score[2]
 score[3]
 
-#14
-a = c(1,2,NA,4,NA,6, 7)
-print(a,na.print="-999")
+# 14.
+a <- c(1, 2, NA, 4, NA, 6, 7)
 
-#15
-name = readline(prompt="Input your name:")
-age = readline(prompt="Input your age:")
-print(paste("My name is", name, "and I am", age, "years old"))
+# a.
+print(a, na.print = "-999")
+
+# b.
+a[is.na(a)] <- 999
+print(a)
+
+# 15.
+name <- readline(prompt = "Input your name: ")
+age <- readline(prompt = "Input your age: ")
+print(paste("My name is", name, "and I am", age, "years old."))
 print(R.version.string)
-
